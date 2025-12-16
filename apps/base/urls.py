4214e5cf44d1d9735a1b4025113a87235abb762f
natali <natali.dnp.ua@gmail.com>
2025-12-16 20:09:22 +0200

    delete files from old project

--- apps/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "base"
-
-urlpatterns = [
-    path("about/", views.AboutUsView.as_view(), name="about_us"),
-]
--- apps/base/urls_root.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.base import views
-
-app_name = "root"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
